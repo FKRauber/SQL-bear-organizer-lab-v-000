a83d2c9ed6bfcf9ca59c25d3e0b7cd31951d14b0
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-05-30 00:50:23 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,9 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
   ("Mr. Chocolate", 3, "M", "brown", "lively", 1),
-  ("Rowdy", 3, "M", "black", "lively", 1),
-  ("Tabitha", 3, "M", "black", "lively", 1),
-  ("Sergeant Brown", 3, "M", "black", "lively", 1),
-  ("Melissa", 3, "M", "black", "lively", 1),
-  ("Grinch", 3, "M", "black", "lively", 1),
-  ("Wendy", 3, "M", "black", "lively", 1),
-  (NULL, 3, "M", "black", "lively", 1);
+  ("Rowdy", 10, "M", "black", "lively", 1),
+  ("Tabitha", 5, "F", "black", "lively", 1),
+  ("Sergeant Brown", 18, "M", "black", "lively", 1),
+  ("Melissa", 7, "F", "black", "lively", 1),
+  ("Grinch", 6, "M", "black", "lively", 1),
+  ("Wendy", 5, "F", "black", "lively", 1),
+  (NULL, 12, "M", "black", "lively", 1);
